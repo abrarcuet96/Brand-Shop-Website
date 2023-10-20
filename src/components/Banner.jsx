a472420a3f183591e2efcd1e49c133bd4dd24245
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-10-20 16:51:13 +0600

    banner section in home page done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/Jskqn63/modern-stationary-collection-arrangement.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-3xl">
+                    <h1 className="mb-5 text-5xl font-bold">Tech Dreams Delivered - Your Ultimate Electronics Destination!</h1>
+                    <p className="mb-5">Welcome to Brand-Shop, where technology meets innovation and quality. We are dedicated to bringing you the latest advancements in the world of electronics and tech gadgets. Dive into our carefully curated collection, featuring cutting-edge smartphones, home automation solutions, and top-notch appliances. Experience the future today as we transform your tech aspirations into reality. Explore our world of endless possibilities and stay at the forefront of innovation!</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../components/Banner";
 import BrandCards from "../components/BrandCardSection/BrandCards";
 
 
 const Home = () => {
     return (
         <div className="max-w-screen-xl mx-auto">
+            <Banner></Banner>
             <BrandCards></BrandCards>
         </div>
     );
