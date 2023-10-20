d156375d625855a8182dd4a524f6fbb7ff95efa2
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-10-20 18:11:26 +0600

    environment variable created

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCIStEIf8lrf3l-WE6qbVj-3iCCQ8riu-g",
-  authDomain: "my-brand-shop-fad71.firebaseapp.com",
-  projectId: "my-brand-shop-fad71",
-  storageBucket: "my-brand-shop-fad71.appspot.com",
-  messagingSenderId: "434233922199",
-  appId: "1:434233922199:web:7ba638220419c288f70fd2"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID, 
+  storageBucket:  import.meta.env.VITE_STORAGEBUCKET, 
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
